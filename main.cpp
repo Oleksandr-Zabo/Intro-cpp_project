89f3aedc12252b2446bc371b78b9afbec605678d
Oleksandr-Zabo <zoleksandr06@gmail.com>
2024-04-18 19:07:53 +0300

    The homework 1 was done()

--- main.cpp
+++ main.cpp
@@ -3,6 +3,10 @@ using namespace std;
 
 int main()
 {
+	cout << "Home Work 1"<<endl;
+	cout << "Name: Gbjhb"<<endl;
+	cout << "email: fbjhb@jhvb" << endl;
+	cout << "Phone: 04567" << endl;
 	system("pause");
 	return 0;
 }
